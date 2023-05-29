407e003b9fe848e65d6d7be7a4b5ac6448520850
nomiyama272 <nomiyamah@aquait.co.jp>
2023-05-29 17:31:37 +0900

    Angular-tutorial-day4

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,7 +14,8 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   
 // private messageService: MessageServiceの部分がチュートリアルだと消えていた
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+// コピペミス？
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
